e3673587ad422fb17b5640c91e77d183e64fbc26
Kyle Lehman <kyle.h.lehman@gmail.com>
2024-08-22 13:07:51 -0400

    Update project workflows

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `polyworld-android-app`
+    `polyworld-android-app-compose`
 }
 
 android {
@@ -21,32 +22,23 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
-
-    implementation(externalLibs.androidx.coreKtx)
-    implementation(externalLibs.androidx.lifecycle.runtimeKtx)
     implementation(externalLibs.androidx.activity.compose)
+    implementation(externalLibs.androidx.compose.material3)
     implementation(externalLibs.androidx.compose.ui)
     implementation(externalLibs.androidx.compose.uiGraphics)
     implementation(externalLibs.androidx.compose.uiToolingPreview)
-    implementation(externalLibs.androidx.compose.material3)
-    testImplementation(externalLibs.junit)
-    androidTestImplementation(externalLibs.androidx.test.extJunit)
-    androidTestImplementation(externalLibs.androidx.test.espressoCore)
-    androidTestImplementation(externalLibs.androidx.compose.uiTestJunit4)
-    debugImplementation(externalLibs.androidx.compose.uiTooling)
+    implementation(externalLibs.androidx.coreKtx)
+    implementation(externalLibs.androidx.lifecycle.runtimeKtx)
+
     debugImplementation(externalLibs.androidx.compose.uiTestManifest)
+    debugImplementation(externalLibs.androidx.compose.uiTooling)
+
+    testImplementation(externalLibs.junit)
+
+    androidTestImplementation(externalLibs.androidx.compose.uiTestJunit4)
+    androidTestImplementation(externalLibs.androidx.test.espressoCore)
+    androidTestImplementation(externalLibs.androidx.test.extJunit)
 }
